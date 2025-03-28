ae97e150172b44259c83ec233bf39a750e2f6c37
root <root@FORGIST.>
2025-03-28 13:16:10 +0300

    task commit

---
+++ task2/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+import redis
+
+app = Flask(__name__)
+
+redis_client = redis.Redis(host='redis', port=6379, decode_responses=True)
+
+@app.route('/ping')
+def ping():
+    return jsonify(status="ok")
+
+@app.route('/count')
+def count():
+    visits = redis_client.incr('counter')
+    return jsonify(count=visits)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
